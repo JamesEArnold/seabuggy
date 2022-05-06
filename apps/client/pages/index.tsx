2b022ad0d674cdbd991de0bb44b1eaf5d0dd6e1b
JamesEArnold <jearnold22@gmail.com>
2022-05-06 14:36:29 -0400

    :truck: SEA-011: rename web to client just cuz

---
+++ apps/client/pages/index.tsx
@@ -0,0 +1,15 @@
+import { Button } from "ui";
+
+export default function Web() {
+  return (
+    <div>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div className="shrink-0 text-slate-500 mx-auto">
+          Welcome to SeaBuggy! 🎉
+        </div>
+      </div>
+      <h1>Web</h1>
+      <Button />
+    </div>
+  );
+}
