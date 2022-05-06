b85b8d1d4b53aa2aac4700d2b480f6b2c7f2a080
JamesEArnold <jearnold22@gmail.com>
2022-05-06 15:05:21 -0400

    :lipstick: SEA-011: add basic notification component to test tailwind

---
+++ packages/ui/Notification.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+export const Notification = () => {
+  return <div className="mx-10 mt-5">
+  <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+    <div className="shrink-0 text-slate-500 mx-auto">
+      Welcome to SeaBuggy! 🎉
+    </div>
+  </div>
+</div>;
+};
