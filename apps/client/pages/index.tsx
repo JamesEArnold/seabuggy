e790faa51d06f375bf68a8df2b6865781cd9f80b
kai-commits <kaimeikle123@gmail.com>
2022-06-10 14:52:40 -0700

    :poop: SEA-14: set dark mode to state and localstorage

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react';
 import { Nav, Notification } from 'ui';
 
 export default function Web() {
+  const getDefaultTheme = (): boolean => {
+    if (typeof window !== 'undefined') {
+      const localTheme: string | null = localStorage.getItem('dark');
+      if (localTheme !== null) {
+         return true;
+      }
+    }
+    return false;
+  };
+
+  const [darkTheme, setDarkTheme] = useState(getDefaultTheme());
+
+  useEffect(() => {
+    localStorage.setItem('dark', JSON.stringify(darkTheme));
+  }, [darkTheme]);
+
   return (
-    <div className="dark">
-      <div className='min-h-screen bg-sea-white-100 dark:bg-sea-blue-500'>
-        <Nav />
+    <div className={darkTheme ? 'light' : 'dark'}>
+      <div className="min-h-screen bg-sea-white-100 dark:bg-sea-blue-500">
+        <Nav setDarkTheme={setDarkTheme} />
         <Notification />
       </div>
     </div>
