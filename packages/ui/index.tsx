06643ab463a8a058acf7c37bbe249515e484c6a7
JamesEArnold <jearnold22@gmail.com>
2022-06-21 20:26:12 -0400

    :fire: SEA-16: burn turborepo to the ground

--- packages/ui/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-export * from "./Button";
-export * from "./Notification";
-export * from "./Nav";
-export * from "./ToggleTheme";
-export * as logo from "./resources/seabuggy_logo.png";
-export * as profilePic from "./resources/profilePictures/profile1.png";
-export * as logoFull from "./resources/seabuggy_logo_full.png";
-export * as logoFullDark from "./resources/seabuggy_logo_full_dark.png";
