a485d8c2a884eb630b3e497cfb2e5d7f5f1be4af
JamesEArnold <jearnold22@gmail.com>
2022-07-12 23:02:57 -0400

    :recycle: SEA-17: refactor get token balance response for ui consumption

--- pages/api/get-token-balance/logic.ts
+++ pages/api/get-token-balance/logic.ts
@@ -1,10 +1,27 @@
+import { TokenBalance } from '@/types';
 import { TokenBalancesResponse } from '@alch/alchemy-web3';
 import { web3alchemy } from '@/utils/web3';
+
+interface TokenBalanceResponse extends TokenBalance {
+  name: string,
+  y: number,
+}
 
 export const getTokenBalances = async (
   walletAddress: string,
   contractAddresses?: string[],
-): Promise<TokenBalancesResponse> =>
-  (contractAddresses !== undefined
-    ? await web3alchemy.getTokenBalances(walletAddress, contractAddresses)
-    : await web3alchemy.getTokenBalances(walletAddress));
+): Promise<TokenBalance[]> => {
+  const { tokenBalances }: TokenBalancesResponse = contractAddresses === undefined
+    ? await web3alchemy.getTokenBalances(walletAddress)
+    : await web3alchemy.getTokenBalances(walletAddress, contractAddresses);
+  return parseTokenBalances(tokenBalances);
+};
+
+const parseTokenBalances = (
+  tokenBalances: TokenBalance[],
+): TokenBalanceResponse[] => tokenBalances.map((value: TokenBalance) => ({
+  ...value,
+  name: value.contractAddress,
+  y: (value.tokenBalance !== null ? parseInt(value.tokenBalance) : 0),
+}));
+
