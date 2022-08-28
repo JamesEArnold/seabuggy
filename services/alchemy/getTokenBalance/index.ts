96bf18bedbda158337ae718c19ec9ff568a32b96
JamesEArnold <jearnold22@gmail.com>
2022-08-28 12:29:51 -0400

    :fire: SEA-18: delete first port adaptor implementation

--- services/alchemy/getTokenBalance/index.ts
+++
@@ -1,7 +0,0 @@
-import { TokenBalances } from '@/ports/getTokenBalances';
-import { alchemy } from '@/services/alchemy';
-
-export const getTokenBalance = async (walletAddress: string): Promise<TokenBalances> => {
-  const response: TokenBalances = await alchemy.core.getTokenBalances(walletAddress);
-  return response;
-};
--- services/alchemy/index.ts
+++
@@ -1,8 +0,0 @@
-import { Alchemy } from 'alchemy-sdk';
-import { alchemyConfig } from '@/configuration';
-import { ethers } from 'ethers';
-export { getTokenBalance } from '@/services/alchemy/getTokenBalance';
-
-export const alchemy: Alchemy = new Alchemy(alchemyConfig);
-
-export const ethersProvider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(`${process.env.ALCHEMY_URL_HTTPS}${process.env.ALCHEMY_API_KEY}`);
