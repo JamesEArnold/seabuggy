da9e5d199b278eeafec169124db28803b35dc587
JamesEArnold <jearnold22@gmail.com>
2022-09-10 14:58:42 -0400

    :memo: SEA-19: add jsdoc snippets to ports for method preview

--- ports/oracles/oracle-repository.ts
+++ ports/oracles/oracle-repository.ts
@@ -1,14 +1,28 @@
 /* eslint-disable no-unused-vars */
 export interface OracleRepository {
-  convertRawTokenPrice: (latestTokenPrice: number, decimalPlaces: number) => number,
 
-  tokenDecimalPlaces: (contractAddress: string) => Promise<number>,
+  /**
+   * Converts the token price to a human readable form based on the
+   * number of decimal places.
+   *
+   * USD denominations should use 8 decimal places (ETH/USD)
+   *
+   * ETH denominations should use 18 decimal places (AAVE/ETH)
+   * @param latestTokenPrice The price of the token to convert
+   * @param decimalPlaces The number of decimal places to account for.
+   */
+  convertRawTokenPrice: (latestTokenPrice: number, decimalPlaces: number) => number,
 
   /**
    * Finds the number of decimal places used to convert
    * the token price into a fixed number.
-   * @param contractAddress The contract address of the token,
-   * @returns {number}
+   * @param contractAddress The contract address of the token
+   */
+  tokenDecimalPlaces: (contractAddress: string) => Promise<number>,
+
+  /**
+   * Finds the latest reported Oracle price of the related token.
+   * @param contractAddress The contract address of the token
    */
   tokenPricingInformation: (contractAddress: string) => Promise<number>,
 }
