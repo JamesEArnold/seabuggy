62ba332606cf5b00931fca5c5492f2b127a970ef
JamesEArnold <jearnold22@gmail.com>
2022-09-10 12:40:27 -0400

    :truck: SEA-19: rename tokens port to infra and add tokenmetadata request

--- ports/tokens/token-repository.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-unused-vars */
-import { TokenBalances } from '@/types/token';
-
-export interface TokenRepository {
-  getTokensByWalletAddress: (walletAddress: string) => Promise<TokenBalances>,
-}
