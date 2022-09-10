5deb091d5680ab9fb70a0623aff2776707bd4c3b
JamesEArnold <jearnold22@gmail.com>
2022-09-10 14:31:53 -0400

    :label: SEA-19: update types to include contract and oracle

---
+++ types/index.ts
@@ -0,0 +1,33 @@
+import {
+  Contract as EthersContract,
+  ContractInterface as EthersContractInterface,
+} from 'ethers';
+import { AlchemyProvider } from 'alchemy-sdk';
+
+export interface TokenBalance {
+  contractAddress: string,
+  tokenBalance: string | null,
+  error: string | null,
+}
+
+export interface TokenBalances {
+  address: string,
+  tokenBalances: TokenBalance[],
+}
+
+export interface TokenMetaData {
+  decimals: number | null,
+  logo: string | null,
+  name: string | null,
+  symbol: string | null,
+}
+
+export type Provider = AlchemyProvider;
+
+export type Contract = EthersContract;
+export type ContractInterface = EthersContractInterface;
+
+export type PricingInformation = {
+  _hex: string,
+  _IsBigNumber: boolean,
+}
