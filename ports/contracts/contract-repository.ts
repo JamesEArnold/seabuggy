f378b7f7891e412c3ab85d7a37c7f9345dea66ce
JamesEArnold <jearnold22@gmail.com>
2022-09-10 14:58:42 -0400

    :memo: SEA-19: add jsdoc snippets to ports for method preview

--- ports/contracts/contract-repository.ts
+++ ports/contracts/contract-repository.ts
@@ -2,6 +2,16 @@
 import { Contract, ContractInterface } from '@/types/index';
 
 export interface ContractRepository {
+
+  /**
+   * Instantiates a Contract using the Infra port provider.
+   *
+   * Methods on the ABI will be callable from the returned Contract.
+   *
+   * What is an ABI? https://www.quicknode.com/guides/solidity/what-is-an-abi
+   * @param contractAddress The address of the Contract to instantiate.
+   * @param abi The ABI of the Contract.
+   */
   instantiateContract: (
     contractAddress: string,
     abi: ContractInterface,
--- ports/infra/infra-repository.ts
+++ ports/infra/infra-repository.ts
@@ -2,7 +2,28 @@
 import { Provider, TokenBalances, TokenMetaData } from '@/types/index';
 
 export interface InfraRepository {
+
+  /**
+   * Instantiates a Read Provider to be used for Contract instantiation.
+   *
+   * @returns { Provider }
+   */
   getProvider: () => Promise<Provider>,
+
+  /**
+   * Returns the TokenBalances that the associated wallet address owns
+   * that are within the top 100 tokens by market cap. Will not return obscure tokens
+   * that are outside of the top 100 market cap.
+   *
+   * @param walletAddress The wallet address to lookup
+   * @returns { TokenBalances }
+   */
   getTokensByWalletAddress: (walletAddress: string) => Promise<TokenBalances>,
+
+  /**
+   * Returns the related TokenMetaData information for the contract address.
+   * Includes decimals, logo, name and symbol.
+   * @param contractAddress The contract address to search for
+   */
   getTokenMetaData: (contractAddress: string) => Promise<TokenMetaData>,
 }
