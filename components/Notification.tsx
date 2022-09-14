31690caa738bc865dfc2a9d5ec39593e3ea7dd20
JamesEArnold <jearnold22@gmail.com>
2022-09-13 20:49:29 -0400

    :dizzy: SEA-20: polish notification adding dismiss timer and animations

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -1,11 +1,76 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
+import { X } from 'react-feather';
+import classNames from 'classnames';
 
-export const Notification = (): ReactElement => (
-  <div className="mx-10 m-5">
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-      <div className="shrink-0 text-slate-500 mx-auto">
-          Welcome to SeaBuggy! 🎉
+interface NotificationProps {
+  content: string,
+  dismissTime: number,
+}
+
+export const Notification = ({
+  content,
+  dismissTime,
+}: NotificationProps): ReactElement => {
+  const [ showAlert, setShowAlert ] = useState(true);
+  const [ slideOut, setSlideOut ] = useState(false);
+  const animateNotificationAndDismiss = () => {
+    setSlideOut(true);
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 1000);
+  };
+
+  setTimeout(() => {
+    if (showAlert === true) {
+      setSlideOut(true);
+      setTimeout(() => {
+        setShowAlert(false);
+      }, 1000);
+    }
+  }, dismissTime);
+
+  useEffect(() => {}, [ showAlert ]);
+
+  return showAlert
+    ? <div
+      onClick={() => animateNotificationAndDismiss()}
+      className={classNames([ slideOut ? 'animate-slideOutTop' : '', 'fixed left-0 right-0 w-80 sm:max-w-md sm:w-1/2 mx-auto flex space-x-2 justify-center cursor-pointer' ])}
+    >
+      <div
+        className="bg-slate-100 shadow-lg mx-auto w-full text-sm pointer-events-auto bg-clip-padding rounded-lg block"
+        id="static-example"
+        role="alert"
+        aria-live="assertive"
+        aria-atomic="true"
+        data-mdb-autohide="false"
+      >
+        <div
+          className=" bg-slate-100 flex flex-row-reverse justify-between items-center py-px px-3 bg-clip-padding border-gray-200 rounded-t-lg"
+        >
+          <div
+            className="flex flex-row-reverse"
+          >
+            <button
+              type="button"
+              className="btn-close w-1 h-1 mr-2 mt-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+              data-mdb-dismiss="toast"
+              aria-label="Close"
+              onClick={() => animateNotificationAndDismiss()}
+            >
+              <X
+                className='h-5'
+                onClick={() => animateNotificationAndDismiss()}
+              />
+            </button>
+          </div>
+        </div>
+        <div className="bg-slate-100 shadow-lg flex flex-col items-center overflow-hidden rounded-xl">
+          <div className="p-3 px-5 text-slate-500 mx-auto">
+            {content}
+          </div>
+          <div className="flex before:content-[''] w-100 h-1 bg-blue-400 -translate-y-0 -translate-x-0 rounded-xl animate-loadingLine"></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+    : <></>;
+};
