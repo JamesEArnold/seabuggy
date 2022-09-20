c51212e0d583d75c8226bf54fe91c74fd1920a9c
JamesEArnold <jearnold22@gmail.com>
2022-09-20 08:58:08 -0400

    :lipstick: SEA-22: add dark and light theme colors for mobile menu

--- components/Nav/MobileMenuButton.tsx
+++ components/Nav/MobileMenuButton.tsx
@@ -4,11 +4,11 @@ import { useState } from 'react';
 export const MobileMenuButton = () => {
   const [ mobileMenu, setMobileMenu ] = useState(false);
 
-  return <div className="absolute inset-y-0 left-0 flex items-center sm:hidden text-sea-blue-300 hover:text-sea-blue-900 dark:text-sea-white-400 dark:hover:text-sea-white-100">
+  return <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
     <a href='#' className="h-8 w-8 flex items-center" onClick={() => setMobileMenu(!mobileMenu)}>
-      <span className={classNames([ mobileMenu ? 'h-0 after:rotate-45 after:top-0 before:-bottom-0.5 before:-rotate-45' : 'h-0.5 after:-top-2 before:-bottom-2', 'bg-white rounded-full w-full absolute transition-all duration-100',
-        'after:bg-white after:rounded-full after:h-0.5 after:w-full after:absolute after:transition-all after:duration-100',
-        'before:bg-white before:rounded-full before:h-0.5 before:w-full before:absolute' ])}></span>
+      <span className={classNames([ mobileMenu ? 'h-0 after:rotate-45 after:top-0 before:-bottom-0.5 before:-rotate-45' : 'h-0.5 after:-top-2 before:-bottom-2', 'dark:bg-sea-white-400 bg-sea-blue-300 rounded-full w-full absolute transition-all duration-100',
+        'after:dark:bg-sea-white-400 after:bg-sea-blue-300 after:rounded-full after:h-0.5 after:w-full after:absolute after:transition-all after:duration-100',
+        'before:dark:bg-sea-white-400 before:bg-sea-blue-300 before:rounded-full before:h-0.5 before:w-full before:absolute' ])}></span>
     </a>
   </div>;
 };
