e0cc5b94495f1dc9b39a1a2ab113641117d9bcff
JamesEArnold <jearnold22@gmail.com>
2022-09-23 16:28:39 -0400

    :heavy_plus_sign: SEA-26: introduce cypress

---
+++ cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  screenshotOnRunFailure: false,
+  video: false,
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    supportFile: false,
+  },
+});
