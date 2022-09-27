9dca2983d1d62cc4e0a11ef1111eaa7c1e04c076
JamesEArnold <jearnold22@gmail.com>
2022-09-27 19:34:26 -0400

    :lipstick: SEA-31: add favicon to head

--- components/index.tsx
+++ components/index.tsx
@@ -4,6 +4,7 @@ export * from './Graph';
 export * from './Notification';
 export * from './Nav/Nav';
 export * from './ToggleTheme';
+export * from './Header';
 export * as logo from './resources/seabuggy_logo.png';
 export * as profilePic from './resources/profilePictures/profile1.png';
 export * as logoFull from './resources/seabuggy_logo_full.png';
