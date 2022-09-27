356437e73f7a909f0b91a8dd0116629d70e0ef7a
JamesEArnold <jearnold22@gmail.com>
2022-09-27 16:17:32 -0400

    :recycle: SEA-28: implement getdatacy helper for getting elements in cypress tests

--- cypress/helpers/index.spec.ts
+++ cypress/helpers/index.spec.ts
@@ -1,8 +1,14 @@
+import { getDataCy, hexToRgb } from '.';
 import { expect } from '@jest/globals';
-import { hexToRgb } from '.';
 
 describe('hexToRgb', () => {
   it('converts a hex code to its rgb equivalent', () => {
     expect(hexToRgb('FFFFFF')).toEqual('rgb(255, 255, 255)');
+  });
+});
+
+describe('getDataCy', () => {
+  it('renders the full getter for data-cy tags', () => {
+    expect(getDataCy('some-data-cy-tag')).toEqual(`[data-cy="some-data-cy-tag"]`);
   });
 });
